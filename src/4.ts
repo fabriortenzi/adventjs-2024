e849c50c0bf2b801c481e5b52b1b2313338cd30a
fabriortenzi <fabriortenzi10@gmail.com>
2024-12-05 11:23:53 -0300

    change 4th challenge

--- src/4.ts
+++ src/4.ts
@@ -1,19 +1,14 @@
 export function createXmasTree(height: number, ornament: string): string {
   if (height < 1 || height > 100) throw 'Height must be between 1 and 100';
 
-  let body = '';
-  for (let i = height; i > 0; i--) {
-    body = body.concat(
-      '_'.repeat(i - 1),
-      ornament.repeat(height - i),
-      ornament,
-      ornament.repeat(height - i),
-      '_'.repeat(i - 1),
-      '\n'
-    );
-  }
+  const totalWidth = 2 * height - 1;
+  const trunk = '_'.repeat((totalWidth - 1) / 2) + '#' + '_'.repeat((totalWidth - 1) / 2);
 
-  const trunk = `${'_'.repeat(height - 1)}#${'_'.repeat(height - 1)}`;
+  const treeBody = Array.from({ length: height }, (_, i) => {
+    const ornaments = ornament.repeat(2 * i + 1);
+    const padding = '_'.repeat((totalWidth - ornaments.length) / 2);
+    return `${padding}${ornaments}${padding}`;
+  }).join('\n');
 
-  return body.concat(trunk, '\n', trunk);
+  return `${treeBody}\n${trunk}\n${trunk}`;
 }
